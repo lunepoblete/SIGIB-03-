c4ea6736c3f10b7cbc80f5651892184014679622
lunepoblete <lunepoblete@gmail.com>
2026-07-29 14:25:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,48 @@
 function abrirModulo(nombre) {
-    alert("Ingresando a módulo: " + nombre);
+
+    if (nombre === "🚒 Equipamiento estructural") {
+
+        mostrarEquipamiento();
+
+    } else {
+
+        alert("Módulo en desarrollo: " + nombre);
+
+    }
+
+}
+
+
+function mostrarEquipamiento() {
+
+    let contenido = "<h2>🚒 Equipamiento estructural</h2>";
+
+    contenido += "<table>";
+
+    contenido += `
+    <tr>
+        <th>Elemento</th>
+        <th>Cantidad</th>
+        <th>Estado</th>
+        <th>Ubicación</th>
+    </tr>
+    `;
+
+    equipamientoEstructural.forEach(item => {
+
+        contenido += `
+        <tr>
+            <td>${item.nombre}</td>
+            <td>${item.cantidad}</td>
+            <td>${item.estado}</td>
+            <td>${item.ubicacion}</td>
+        </tr>
+        `;
+
+    });
+
+    contenido += "</table>";
+
+    document.querySelector("main").innerHTML = contenido;
+
 }
